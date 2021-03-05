82e685b25e48e2d7cdc813990e3fb0dd3db903bc
Sebastian Hasait <sebastian@hasait.de>
2021-03-05 23:02:35 +0100

    Update prepare_system.sh
    Ubuntu 20.04 - python3

--- prepare_system.sh
+++ prepare_system.sh
@@ -16,8 +16,8 @@ install_pip () {
 prepare_ubuntu() {
         $SUDO apt update -y
         $SUDO apt dist-upgrade -y
-        $SUDO apt install software-properties-common curl git mc vim facter python python-apt aptitude -y
-        [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python-dev libffi-dev libssl-dev make -y
+        $SUDO apt install software-properties-common curl git mc vim facter python3 python3-apt aptitude -y
+        [ $(uname -m) == "aarch64" ] && $SUDO apt install gcc python3-dev libffi-dev libssl-dev make -y
 
         PYTHON_BIN=/usr/bin/python
         install_pip
